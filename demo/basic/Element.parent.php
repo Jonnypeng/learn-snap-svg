78bf796d0492a3d050f96ee1bcb2f361f12640bc
jonny <jonny@jonnydeMacBook-Pro.local>
2016-11-18 11:59:50 +0800

    goon

--- demo/basic/Element.parent.php
+++ demo/basic/Element.parent.php
@@ -1,55 +1,26 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<?php 
-	$key = 'Element.parent()';
-	include('../_include/header.php'); 
-?>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title></title>
+<style>
+	body{
+	margin:0px;
+	}
+	svg{
+		position:fixed;
+		left:0px;
+		top:0px;
+	}
+</style>
 </head>
-
 <body>
-<?php 
-	include('../_include/nav.php'); 
-?>
-
-<div id="zxxBody" class="zxx_body">
-	<div id="zxxSlide" class="zxx_side">
-    	<?php include('../_include/slide.php'); ?>
-    </div>
-    <div class="zxx_api_detail">
-    	<div class="zxx_api_content">
-        	<h1 class="zxx_api_h1"><sub>Snap.svg demo之</sub><?php echo $key; ?></h1>
-        	<h2>Element.parent()</h2>
-        	<p>返回元素的父元素。</p>
-            <h3>返回值</h3>
-            <p>元素。父元素。</p>
-            <h3>使用</h3>
-            <p>改变圆圈圈父元素的<code>id</code>.</p>
-            <pre>&lt;svg id="svg" width="100" height="100">&lt;/svg></pre>
-            <pre>var svg = Snap("#svg");
-var c = svg.paper.circle(50, 50, 40);
-c.<mark>parent()</mark>.attr({
-    id: "custom"			
-});</pre>
-            <h3>效果</h3>
-            <p>
-            	<svg id="svg" width="100" height="100"></svg>                
-            </p>
-            <h3>生成HTML</h3>
-            <p><img src="../../static/img/demo-snap-svg-24.png"><tip data-content="截图"></tip></p>
-        </div>
-    </div>
-</div>
-
-<?php 
-	include('../_include/footer.php'); 
-?>
+<svg id="svg" width="100%" height="100%">
+	circle[cx="150"]
+</svg>
+<script src="../../static/snap.svg-min.js"></script>
 <script>
-var svg = Snap("#svg");
-var c = svg.paper.circle(50, 50, 40);
-c.parent().attr({
-	id: "custom"			
-});
 </script>
 </body>
 </html>
--- demo/basic/Element.select.php
+++ demo/basic/Element.select.php
@@ -1,61 +1,35 @@
-<!doctype html>
-<html>
+<html lang="en">
 <head>
-<?php 
-	$key = 'Element.select()';
-	include('../_include/header.php'); 
-?>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title></title>
+<style>
+	body{
+	margin:0px;
+	}
+	svg{
+		position:fixed;
+		left:0px;
+		top:0px;
+	}
+</style>
 </head>
-
 <body>
-<?php 
-	include('../_include/nav.php'); 
-?>
-
-<div id="zxxBody" class="zxx_body">
-	<div id="zxxSlide" class="zxx_side">
-    	<?php include('../_include/slide.php'); ?>
-    </div>
-    <div class="zxx_api_detail">
-    	<div class="zxx_api_content">
-        	<h1 class="zxx_api_h1"><sub>Snap.svg demo之</sub><?php echo $key; ?></h1>
-        	<h2>Element.select(query)</h2>
-        	<p>采集匹配CSS选择器的内嵌元素。</p>
-            <h3>参数</h3>
-            <ul>
-            	<li><code>query</code> 字符串。CSS选择器。</li>
-            </ul>
-            <h3>返回值</h3>
-            <p>元素。选择的元素。</p>
-            <h3>使用</h3>
-            <p>选择<code>&lt;circle&gt;</code>元素（只是第1个）并让其脸红。</p>
-            <pre>&lt;svg id="svg" width="200" height="100">
-    &lt;circle cx="50" cy="50" r="40">&lt;/circle>
-    &lt;circle cx="150" cy="50" r="40">&lt;/circle>        
-&lt;/svg></pre>
-            <pre>var svg = Snap("#svg");
-svg.<mark>select</mark>("circle").attr({
-    fill: "#f00"					  
-});</pre>
-            <h3>效果</h3>
-            <p>
-            	<svg id="svg" width="200" height="100">
-                	<circle cx="50" cy="50" r="40"></circle>
-                    <circle cx="150" cy="50" r="40"></circle>
-                </svg>                
-            </p>
-        </div>
-    </div>
-</div>
-
-<?php 
-	include('../_include/footer.php'); 
-?>
+<svg id="svg" width="100%" height="100%">
+	<circle id="c1" cx="50" cy="50" r="25" fill="red"></circle>
+	<circle id="c2" cx="50" cy="100" r="25" fill="red"></circle>
+</svg>
+<script src="../../static/snap.svg-min.js"></script>
 <script>
-var svg = Snap("#svg");
-svg.select("circle").attr({
-	fill: "#f00"					  
-});
+	var svg = Snap("#svg");
+	svg.select("circle").animate({
+"fill":"blue",
+"r":"12.5",
+"cy":"100"
+	},2000,mina.bounce)
+	var c1 = Snap("#c1");
+	var c2 = Snap("#c2");
+	c2.after(c1);
 </script>
 </body>
 </html>
