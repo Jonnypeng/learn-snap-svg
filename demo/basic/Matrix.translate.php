d632bfad9e076224ececd17efc3cf23c447f522d
jonny <jonny@jonnydeMacBook-Pro.local>
2016-11-21 21:42:16 +0800

    m

--- demo/basic/Matrix.translate.php
+++ demo/basic/Matrix.translate.php
@@ -1,72 +1,36 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<?php 
-	$key = 'Matrix.translate()';
-	include('../_include/header.php'); 
-?>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title></title>
+<style>
+	body{
+	margin:0px;
+	}
+	svg{
+		position:fixed;
+		left:0px;
+		top:0px;
+	}
+</style>
 </head>
-
 <body>
-<?php 
-	include('../_include/nav.php'); 
-?>
-
-<div id="zxxBody" class="zxx_body">
-	<div id="zxxSlide" class="zxx_side">
-    	<?php include('../_include/slide.php'); ?>
-    </div>
-    <div class="zxx_api_detail">
-    	<div class="zxx_api_content">
-        	<h1 class="zxx_api_h1"><sub>Snap.svg demo之</sub><?php echo $key; ?></h1>
-        	<h2>Matrix.translate(x, y)</h2>
-        	<p>偏移矩阵。</p>
-            <h3>参数</h3>
-            <ul>
-                <li><code>x</code> 数值。表示<code>x</code>方向偏移距离。</li>
-                <li><code>y</code> 数值。表示<code>y</code>方向偏移距离。</li>
-            </ul>
-            <h3>使用</h3>
-            <pre>&lt;svg id="svg" width="100" height="100">&lt;/svg>
-
-&lt;input id="button" type="button" class="zxx_api_button" value="点击右下偏移10像素"></pre>
-            <pre>var svg = Snap("#svg");
-var c = svg.paper.rect(20, 20, 60, 60, 10).attr({
-    fill: "#f00"    <span class="comment">// 红色</span>
-});
-
-document.getElementById("button").onclick = function() {
-    var m = new Snap.Matrix();
-    m.<mark>translate</mark>(10, 10);
-    <span class="comment">// 旋转</span>
-    c.transform(m);
-};</pre>
-            <h3>效果</h3>
-            <p>
-            	<svg id="svg" width="100" height="100"></svg>                
-            </p>
-            <p><input id="button" type="button" class="zxx_api_button" value="点击右下偏移10像素"></p>
-            <h3>首次旋转后HTML</h3>
-            <p><img src="../../static/img/demo-snap-svg-34.png"><tip data-content="截图"></tip></p>
-        </div>
-    </div>
-</div>
-
-<?php 
-	include('../_include/footer.php'); 
-?>
+<svg id="svg" width="100%" height="100%"></svg>
+<script src="../../static/snap.svg-min.js"></script>
 <script>
-var svg = Snap("#svg");
-var c = svg.paper.rect(20, 20, 60, 60, 10).attr({
-    fill: "#f00"    // 红色
-});
-
-document.getElementById("button").onclick = function() {
-    var m = new Snap.Matrix();
-    m.translate(10, 10);
-    // 旋转
-    c.transform(m);
-};
+	var svg = Snap("#svg");	
+	var c1 = svg.paper.circle({
+	"cx":"50",
+	"cy":"50",
+	"r":"40"
+	}).attr({
+	"fill":"red"
+	});;
+	var m = new Snap.Matrix();
+	c1.animate({
+"transform":m.translate(100,100)
+	},1000,mina.bounce);
 </script>
 </body>
 </html>
--- demo/basic/Snap.animation.php
+++ demo/basic/Snap.animation.php
@@ -1,47 +1,25 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<?php 
-	$key = 'Snap.animation()';
-	include('../_include/header.php'); 
-?>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title></title>
+<style>
+	body{
+	margin:0px;
+	}
+	svg{
+		position:fixed;
+		left:0px;
+		top:0px;
+	}
+</style>
 </head>
-
 <body>
-<?php 
-	include('../_include/nav.php'); 
-?>
-
-<div id="zxxBody" class="zxx_body">
-	<div id="zxxSlide" class="zxx_side">
-    	<?php include('../_include/slide.php'); ?>
-    </div>
-    <div class="zxx_api_detail">
-    	<div class="zxx_api_content">
-        	<h1 class="zxx_api_h1"><sub>Snap.svg demo之</sub><?php echo $key; ?></h1>
-        	<h2>Snap.animation(attr, duration, [easing], [callback])</h2>
-        	<p>创建一个<code>animation</code>对象。</p>
-            <h3>参数</h3>
-            <ul>
-            	<li><code>attr</code> 对象。最终目标的属性。</li>
-                <li><code>duration</code> 数值。动画持续的时间，单位为毫秒。</li>
-                <li><code>easing</code> 函数。<code>mina</code>或自定义的缓动函数。</li>
-                <li><code>callback</code> 函数。动画结束的时候执行的回调函数。</li>
-            </ul>
-            <h3>返回值</h3>
-            <p>对象。动画对象。</p>
-            <h3>使用</h3>
-            <p style="color:#999;">//zxx: 应用场景不明，使用不详。</p>
-        </div>
-    </div>
-</div>
-
-<?php 
-	include('../_include/footer.php'); 
-?>
+<svg id="svg" width="100%" height="100%"></svg>
+<script src="../../static/snap.svg-min.js"></script>
 <script>
-window.console &&
-console.log(Snap.animation({ x: 30 }, 1000));
+						
 </script>
 </body>
 </html>
